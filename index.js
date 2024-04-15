4a18e08d556c73409a043acd190d850ee098a201
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-04-15 15:23:23 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+f/*unction submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        return data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        return error;
+    });
+}*/
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Unauthorized Access');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        document.body.innerHTML += data.id;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.body.innerHTML += error.message;
+    });
+}
+
